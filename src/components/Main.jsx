19c205770e0aadc6c60b56baf6ba66310874ff48
rpeterson87 <oshorhino5053@gmail.com>
2022-09-16 09:43:09 -0400

    url change

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, Outlet } from "react-router-dom"
 import People from "../pages/People"
 import Show from "../pages/Show"
 const Main = () => {
-  const URL = "https://mern-fully-working.herokuapp.com/people"
+  const URL = "https://mern-fully-working.herokuapp.com/people/"
   return (
     <main>
         <Outlet/>
